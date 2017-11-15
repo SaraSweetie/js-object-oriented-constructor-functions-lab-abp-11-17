02c9f745ec390c07ed9fe5f90115c7d7187aadff
Sara Smith <sarasmith887@gmail.com>
2017-11-15 23:36:04 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+constructor Scooter (year, color, model){
+  this.year = year
+  this.color = color
+  this.model = model
+}
+
+constructor Driver 
+name
+age
+experience
+
+
+
+PickupLocation
+address
+city
