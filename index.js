59b6917297e580087c2f47f4489813a305c100ae
Sara Smith <sarasmith887@gmail.com>
2017-11-15 23:48:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function Driver(name, age, experience) {
   this.experience = experience;
 }
 
-function PickupLocation(addres, city) {
+function PickupLocation(address, city) {
     this.address = address;
     this.city = city;
 }
